a267fcf4258c44d174c3737c3bb5392b2f639a87
fvs <fvs@30e9dc4a-7611-4fbc-83ba-eecf79b76bd0>
2010-10-21 13:52:00 +0000

    pickUpAgencySubdivision
    git-svn-id: svn://oss.dbc.dk/sync_php/php/OpenLibrary/OpenOrder/trunk@26239 30e9dc4a-7611-4fbc-83ba-eecf79b76bd0

--- server.php
+++ server.php
@@ -140,7 +140,8 @@ class openOrder extends webServiceServer {
         $this->add_ubf_node($ubf, $order, "orderId", $param->orderId->_value);		// ??
         $this->add_ubf_node($ubf, $order, "orderSystem", $param->orderSystem->_value);
         $this->add_ubf_node($ubf, $order, "pagination", $param->pagination->_value);
-        $this->add_ubf_node($ubf, $order, "pickUpAgencyId", $param->pickUpAgencyId->_value);		// ??
+        $this->add_ubf_node($ubf, $order, "pickUpAgencyId", $param->pickUpAgencyId->_value);
+        $this->add_ubf_node($ubf, $order, "pickUpAgencySubdivision", $param->pickUpAgencySubdivision->_value);
         $this->add_ubf_node($ubf, $order, "placeOfPublication", $param->placeOfPublication->_value);		// ??
         $this->add_ubf_node($ubf, $order, "publicationDate", $param->publicationDate->_value);
         $this->add_ubf_node($ubf, $order, "publicationDateOfComponent", $param->publicationDateOfComponent->_value);
